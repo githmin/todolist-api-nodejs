dd0ba0c641b17dba6fe2877fdbe63649a0834d97
Githmin Jayawardhana <Githmin21@gmail.com>
2023-03-10 11:50:00 +0530

    added update note path

--- controller/noteController.js
+++ controller/noteController.js
@@ -41,4 +41,22 @@ router.delete("/", async (req, res) => {
     });
 });
 
+router.put("/", async (req, res) => {
+  await todoNote
+    .findByIdAndUpdate(req.body.id, { note: req.body.note })
+    .then(
+      res.send({
+        status: 200,
+        params: "Save Successfull",
+      })
+    )
+    .catch((e) => {
+      res.send({
+        status: 400,
+        params: "Save Unsuccesfull",
+      });
+      console.log(e);
+    });
+});
+
 module.exports = router;
